f75030c727f64d31d70d069c96352b89825810bb
Prateek Sahay <ptkinvent@gmail.com>
2026-03-09 21:58:04 -0400

    Added scaffolding

---
+++ README.md
@@ -0,0 +1,14 @@
+# Job Goblin
+
+This project is a Next.js web app for helping people study and prepare for job interviews.
+
+## Configuration
+
+Create a file called .env:
+
+```
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=...
+CLERK_SECRET_KEY=...
+NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
+NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+```
---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function MainLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <meta charSet="UTF-8" />
+          <title>Job Goblin</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+          <meta name="description" content="Interview prep and coaching tool" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </head>
+        <body className="h-full bg-white dark:bg-secondary-800">
+          <ThemeProvider attribute="class" enableSystem={false}>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
